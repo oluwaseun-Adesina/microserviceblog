a86134139323a429e1b003c19ca1c78f91b1ec6c
AmosSpark <ayobamiolugboyega@gmail.com>
2023-05-12 16:32:54 +0100

    structure
    + modify contrller and routes

--- users/app.js
+++ users/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const routes = require("./src/routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.use("/api", routes);
 
 module.exports = app;
